a5857accd6577f8ea51acf2b926dab4fccef5ad5
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2022-10-24 17:37:53 +0300

    creazione div e logica

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 "use strict";
 const fizz = "fizz";
 const buzz = "buzz";
-
+const numberContainer = document.getElementById('number-container');
 
 for(let i=1; i<=100; i++){
     if(i%15 === 0){
-        console.log(fizz + buzz);
+        const boardNumber = document.createElement('div');
+        boardNumber.classList.add('box');
+        boardNumber.classList.add('fizzbuzz');
+        numberContainer.append(boardNumber);
+        boardNumber.innerHTML = fizz + buzz;
+        // console.log(fizz + buzz);
     }else if(i%3 === 0){
-        console.log(fizz);
+        const boardNumber = document.createElement('div');
+        boardNumber.classList.add('box');
+        boardNumber.classList.add('fizz');
+        numberContainer.append(boardNumber);
+        boardNumber.innerHTML = fizz;
+        // console.log(fizz);
     }else if(i%5 === 0){
-        console.log(buzz);
+        const boardNumber = document.createElement('div');
+        boardNumber.classList.add('box');
+        boardNumber.classList.add('buzz');
+        numberContainer.append(boardNumber);
+        boardNumber.innerHTML = buzz;
+        // console.log(buzz);
     }else{
-        console.log(i)
+        const boardNumber = document.createElement('div');
+        boardNumber.classList.add('box');
+        numberContainer.append(boardNumber);
+        boardNumber.innerHTML = i;
+        // console.log(i);
     }
 }
